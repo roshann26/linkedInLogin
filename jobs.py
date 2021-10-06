a91606b39bcb2d467ab06a590f2e459ebddccc16
kumar <terawork9@gmail.com>
2021-10-06 23:20:51 +0530

    adding reference for next task.

---
+++ jobs.py
@@ -0,0 +1,3 @@
+#https://maoviola.medium.com/a-complete-guide-to-web-scraping-linkedin-job-postings-ad290fcaa97f
+
+#https://medium.com/nerd-for-tech/linked-in-web-scraper-using-selenium-15189959b3ba
